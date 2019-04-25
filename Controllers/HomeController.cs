e5114a68cd3b169414bc8527b1684fe4c3833a54
Marko Žlogar <marko.zlogar@gmail.com>
2019-04-25 15:09:36 +0200

    initial project setup

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DockerLearn.Models;
+using DockerLearn.Context;
+
+namespace DockerLearn.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Articles()
+        {
+            var model = new ArticlesViewModel();
+            model.Articles = new ArticleRepository().GetLatest();
+
+            return View(model);
+        }
+    }
+}
